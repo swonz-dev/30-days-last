b5ce065c663aa8997bef2e562a23d01a96a18621
Oliver Schwanz <oliver@swonz.dev>
2024-07-07 16:15:25 +0200

    Day 6: added tailwind and added a form renderer

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    TalesFromADev\Twig\Extra\Tailwind\Bridge\Symfony\Bundle\TalesFromADevTwigExtraTailwindBundle::class => ['all' => true],
+    TalesFromADev\FlowbiteBundle\TalesFromADevFlowbiteBundle::class => ['all' => true],
 ];
